c6a6f00b9b09eb091d354a9ba4a1c82a1e0446da
VID Neto <dello5@yandex.ru>
2023-03-22 19:38:59 +0300

    mistake resolved

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-    int account = 14000;
-    int payment = 1010;
+        int account = 14000;
+        int payment = 1050;
 
-    int bonus ;
-       if (payment >= 1100) {
-        bonus = payment / 100;
-    } else {
-        bonus = 0;
-       }
+        int bonus;
+        if (payment >= 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
 
-   int finalAccount = account + payment + bonus;
+        int finalAccount = account + payment + bonus;
 
 
         System.out.println("Количество бонусных рублей = " + bonus);
